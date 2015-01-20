2b3c49608d5b9a07563ef88728cd77e8b6f6e3bc
Chris Madrigal <crismas07@gmail.com>
2015-01-20 10:32:56 -0800

    Animate scroll for header links.

--- js/main.js
+++ js/main.js
@@ -1,19 +1,34 @@
 $(function() {
+	// Animate scroll for header links.
+	$('a', '#lineup').on('click', function (e) {
+		var animating;
+		e.preventDefault();
+
+		if (animating) return;
+		var id = $(e.currentTarget).attr('href').match(/#.+/)[0];
+		animating = true;
+
+		$('html,body').stop().animate({scrollTop: $(id).offset().top}, 300, function() {
+			animating = false;
+		});
+	});
+
+
+	// Open / collapse readmore.
 	$('.read-more').click(function (e) {
 		e.preventDefault();
-		$el = $(e.currentTarget);
+		var $el = $(e.currentTarget);
 
 		// Toggle open class on click.
 		$el.siblings('.more').toggleClass('open');
-		
+
 		// Toggle message
         if ($el.data('open') == true) {
-          $el.html('Read Full Bio').data("open", false);
+			$el.html('Read Full Bio').data("open", false);
         }
         else {
-          $el.html('Collapse Bio').data("open", true);
+			$el.html('Collapse Bio').data("open", true);
         }
-		
 	});
 });
 
